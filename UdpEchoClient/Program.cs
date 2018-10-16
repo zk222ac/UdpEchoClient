f28c1d8f93d324f2da702df2a752f75e0bee063c
Zuhair Haroon Khan <zk222ac@gmail.com>
2018-10-17 00:13:40 +0200

    final release

--- UdpEchoClient/Program.cs
+++ UdpEchoClient/Program.cs
@@ -1,8 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace UdpEchoClient
 {
@@ -10,12 +10,49 @@ namespace UdpEchoClient
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            try
+            {
+                // Send data over UDP packet 
+                // source ip address and source port
+                int sourceListenPort = 9999;
+                UdpClient udpClient = new UdpClient("127.0.0.1", sourceListenPort);
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+                // IPEndPoint --> connect to server : you need to
+                // Destination IP address
+                // Destination Port
+                IPAddress destinationIpAddress = IPAddress.Loopback;
+                int DestinationListenPort = 9999;
+                IPEndPoint remoteIpEndPoint = new IPEndPoint(destinationIpAddress, DestinationListenPort);
+                //udpClient.Connect(remoteIpEndPoint);
+                // we need to convert data into bytes
+                Console.WriteLine("Write your Message here ");
+                string msg = Console.ReadLine();
+                int n = 0;
+                for (int i = 0; i < 1000; i++)
+                {
+                    Byte[] sendBytes = Encoding.ASCII.GetBytes($"{msg}-number :{n}");
+                    // UDP ready to send byte data to host at the specified remote endpoint 
+                    udpClient.Send(sendBytes, sendBytes.Length);
+
+                    // Client received  bytes from Server 
+                    Byte[] receivedBytes = udpClient.Receive(ref remoteIpEndPoint);
+
+                    string receivedMsg = Encoding.ASCII.GetString(receivedBytes);
+                    Console.WriteLine($" Message from Server:{receivedMsg}");
+                    
+                    n++;
+                }
+
+                Console.WriteLine("Now loop is finished");
+                Console.ReadKey();
+            
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadLine();
+            }
+
         }
     }
 }
